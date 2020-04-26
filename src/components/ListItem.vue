fdb73e4433ef574aab6fa4a4ca4b2422306dc5af
Adrian Dąbek <adrian950113@gmail.com>
2020-04-26 14:12:06 +0200

    add count to list

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,17 +1,33 @@
 <template>
-  <li>{{ item }}</li>
+  <li>
+    <span class="name">{{ item }}</span>
+    <span class="count">
+      <fa-i icon="minus" @click=decrement></fa-i>
+      {{ count }}
+      <fa-i icon="plus" @click=increment></fa-i>
+    </span>
+  </li>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "@vue/composition-api";
+import { defineComponent, ref } from "@vue/composition-api";
 import { db } from "../firestore";
 export default defineComponent({
   name: "ListItem",
   props: {
-    item: String,
+    item: String
   },
   setup() {
-    return {};
+    const count = ref(1);
+    return {
+      count,
+      increment() {
+        count.value += 1;
+      },
+      decrement() {
+        count.value -= 1;
+      }
+    };
   }
 });
 </script>
@@ -19,5 +35,8 @@ export default defineComponent({
 <style scoped>
 li {
   text-align: left;
+}
+.count {
+  float: right;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import App from "./App.vue";
 import VueCompositionApi from '@vue/composition-api';
 import "./registerServiceWorker";
 import { firestorePlugin } from 'vuefire';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlus, faMinus, faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.config.productionTip = false;
+library.add(faPlus, faMinus, faChevronDown, faChevronUp);
+
 Vue.use(VueCompositionApi);
 Vue.use(firestorePlugin);
+Vue.component('fa-i', FontAwesomeIcon)
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App)
