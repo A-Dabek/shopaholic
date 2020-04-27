2c3b3922de99fb850051f514e4265e9076bf585e
Adrian Dąbek <adrian950113@gmail.com>
2020-04-27 20:13:02 +0200

    add reordering

--- src/components/Shopping.vue
+++ src/components/Shopping.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <ol>
+    <draggable :value="distinctItems" tag="ol" @change="onReorder">
       <li
         v-for="(item, index) of distinctItems"
         :key="item"
@@ -12,7 +12,7 @@
         {{item}}
         <span class="count">{{itemCount[item]}}</span>
       </li>
-    </ol>
+    </draggable>
   </div>
 </template>
 
@@ -22,32 +22,39 @@ import {
   reactive,
   ref,
   onMounted,
-  computed
+  computed,
+  onUnmounted,
+  set
 } from "@vue/composition-api";
 import { db } from "../firestore";
 import { countBy, flatten, orderBy } from "lodash-es";
+import draggable from "vuedraggable";
 
 export default defineComponent({
   name: "Shopping",
+  components: {
+    draggable
+  },
   firestore: {
     shop: db.collection("shops").doc("first shop"),
     bought: db.collection("bought"),
     planned: db.collection("planner")
   },
   setup() {
-    const bought = ref<{ id: string }[]>([]);
-    const planned = ref([]);
     const shop = ref<{ [k: string]: number }>({});
+
+    const planned = ref([]);
     const itemCount = computed(() => countBy(planned.value));
     const distinctItems = computed(() =>
       orderBy(Object.keys(itemCount.value), x => shop.value[x])
     );
+
+    const bought = ref<{ id: string }[]>([]);
     const boughtDict = computed<{ [k: string]: boolean }>(() =>
       bought.value
         .map(data => data.id)
         .reduce((prev, curr) => ({ ...prev, [curr]: true }), {})
     );
-
     function onItemClick(item: string) {
       if (boughtDict.value[item]) {
         db.collection("bought")
@@ -58,6 +65,28 @@ export default defineComponent({
           .doc(item)
           .set({});
       }
+    }
+
+    function onReorder(event: {
+      moved: { element: string; oldIndex: number; newIndex: number };
+    }) {
+      const orderedEntries = orderBy(Object.entries(shop.value), x => x[1]);
+      const pushToLast = event.moved.newIndex >= orderedEntries.length;
+      const movingForwards = event.moved.newIndex > event.moved.oldIndex;
+      const newItemTuple: [string, number] = [
+        event.moved.element,
+        pushToLast ? orderedEntries.length : event.moved.newIndex
+      ];
+      const newOrderedEntries = orderedEntries
+        .filter(([key]) => key !== event.moved.element)
+        .flatMap((item, index) =>
+          item[1] === event.moved.newIndex ? (movingForwards ? [item, newItemTuple] : [newItemTuple, item]) : [item]
+        )
+        .map(([key], index) => [key, index]);
+      const newShopOrder = Object.fromEntries(pushToLast ? [...newOrderedEntries, newItemTuple] : newOrderedEntries);
+      db.collection("shops")
+        .doc("first shop")
+        .set(newShopOrder);
     }
 
     onMounted(() => {
@@ -76,7 +105,8 @@ export default defineComponent({
       itemCount,
       distinctItems,
       boughtDict,
-      onItemClick
+      onItemClick,
+      onReorder
     };
   }
 });
