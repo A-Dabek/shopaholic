7480f2b9bf7a1851559b0d06a02907a8b10c0222
Adrian Dąbek <adrian950113@gmail.com>
2020-09-11 21:25:52 +0200

    improve reordering

--- src/components/Shopping.vue
+++ src/components/Shopping.vue
@@ -4,13 +4,29 @@
       <li
         v-for="(item, index) of distinctItems"
         :key="item"
+        class="shopping-item"
         :class="{bought: boughtDict[item]}"
-        @click="() => onItemClick(item)"
       >
-        <fa-i v-if="shop[item] === undefined" icon="question"></fa-i>
-        <span v-else>{{index + 1}}.</span>
-        {{item}}
-        <span class="count">{{itemCount[item]}}</span>
+        <div class="shopping-item-wrapper" @click="() => onItemClick(item)">
+          <fa-i v-if="shop[item] === undefined" icon="question"></fa-i>
+          <span v-else>{{index + 1}}.</span>
+          {{item}}
+          <span class="count">{{itemCount[item]}}</span>
+        </div>
+
+        <ol v-if="item === itemReordered">
+          <li @click="reorder(item, itemsBetween[0], false)">{{index + 1}}. Pierwszy</li>
+          <li
+            v-for="(nestedItem, nestedIndex) of itemsBetween"
+            :key="nestedItem"
+            class="reorder-list-item"
+          >
+            <div class="order-item-wrapper">
+              <label @click="reorder(item, nestedItem)">{{index + nestedIndex + 2}}. {{nestedItem}}</label>
+              <fa-i class="remove-item" icon="times" @click="removeFromList(nestedItem)"></fa-i>
+            </div>
+          </li>
+        </ol>
       </li>
     </draggable>
   </div>
@@ -24,7 +40,7 @@ import {
   onMounted,
   computed,
   onUnmounted,
-  set
+  set,
 } from "@vue/composition-api";
 import { db } from "../firestore";
 import { countBy, flatten, orderBy } from "lodash-es";
@@ -37,15 +53,15 @@ export default defineComponent({
   props: {
     shopName: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   components: {
-    draggable
+    draggable,
   },
   firestore: {
     bought: db.collection("bought"),
-    planned: db.collection("planner")
+    planned: db.collection("planner"),
   },
   setup(props) {
     const shop = ref<{ [k: string]: number }>({});
@@ -55,7 +71,7 @@ export default defineComponent({
       unsubscribe = db
         .collection("shops")
         .doc(props.shopName)
-        .onSnapshot(snapshot => {
+        .onSnapshot((snapshot) => {
           shop.value = snapshot.data() as any;
         });
     });
@@ -67,27 +83,39 @@ export default defineComponent({
     const planned = ref<string[]>([]);
     const itemCount = computed(() => countBy(planned.value));
     const distinctItems = computed(() =>
-      orderBy(Object.keys(itemCount.value), x => shop.value[x])
+      orderBy(Object.keys(itemCount.value), (x) => shop.value[x])
     );
 
     const bought = ref<{ id: string }[]>([]);
     const boughtDict = computed<{ [k: string]: boolean }>(() =>
       bought.value
-        .map(data => data.id)
+        .map((data) => data.id)
         .reduce((prev, curr) => ({ ...prev, [curr]: true }), {})
     );
 
+    const itemReordered = ref<string>("");
+    const itemsBetween = ref<string[]>([]);
+
     return {
+      itemReordered,
+      itemsBetween,
       bought,
       planned,
       shop,
       itemCount,
       distinctItems,
       boughtDict,
-      ...useShoppingLogic(props.shopName, planned, shop, distinctItems),
-      ...useBoughtLogic(boughtDict)
+      ...useShoppingLogic(
+        props.shopName,
+        planned,
+        shop,
+        distinctItems,
+        itemReordered,
+        itemsBetween
+      ),
+      ...useBoughtLogic(boughtDict),
     };
-  }
+  },
 });
 </script>
 
@@ -109,23 +137,23 @@ li {
   padding-right: 5px;
   padding-left: 5px;
 }
-li:nth-child(10n + 1) {
+li.shopping-item:nth-child(10n + 1) div.shopping-item-wrapper {
   background: #dce775;
   --var-color: #dce775;
 }
-li:nth-child(10n + 3) {
+li.shopping-item:nth-child(10n + 3) div.shopping-item-wrapper {
   background: #4db6ac;
   --var-color: #4db6ac;
 }
-li:nth-child(10n + 5) {
+li.shopping-item:nth-child(10n + 5) div.shopping-item-wrapper {
   background: #a1887f;
   --var-color: #a1887f;
 }
-li:nth-child(10n + 7) {
+li.shopping-item:nth-child(10n + 7) div.shopping-item-wrapper {
   background: #f3cc74;
   --var-color: #f3cc74;
 }
-li:nth-child(10n + 9) {
+li.shopping-item:nth-child(10n + 9) div.shopping-item-wrapper {
   background: #e08a8a;
   --var-color: #e08a8a;
 }
@@ -135,5 +163,18 @@ li.bought {
 .count {
   float: right;
   font-weight: bold;
+}
+.order-item-wrapper {
+  display: flex;
+  align-items: baseline;
+}
+.order-item-wrapper label {
+  flex-grow: 1;
+}
+.remove-item {
+  width: 15%;
+  align-self: right;
+  flex-grow: 0;
+  margin-left: 15px;
 }
 </style>
--- src/components/useShoppingLogic.ts
+++ src/components/useShoppingLogic.ts
@@ -10,44 +10,82 @@ export function useShoppingLogic(
   shopName: string,
   planned: Ref<string[]>,
   shop: Ref<{ [k: string]: number }>,
-  distinctItems: Ref<readonly string[]>
+  distinctItems: Ref<readonly string[]>,
+  itemReordered: Ref<string>,
+  itemsBetween: Ref<string[]>
 ) {
   onMounted(() => {
     db.collection("planner")
       .get()
       .then(snapshot => {
         planned.value = flatten(snapshot.docs.map(doc => doc.data().items));
-      });
+      });0
   });
+
+  function reset() {
+    itemReordered.value = "";
+    itemsBetween.value = [];
+  }
 
   function onReorder(event: {
     moved: { element: string; oldIndex: number; newIndex: number };
   }) {
     const distinctOrderedItems = distinctItems.value.filter(x => shop.value[x] != null)
     const orderedEntries = orderBy(Object.entries(shop.value), x => x[1]);
-    const pushToLast = event.moved.newIndex >= distinctOrderedItems.length;
-    const newItemTuple: [string, number] = [
-      event.moved.element,
-      pushToLast ? distinctOrderedItems.length : event.moved.newIndex
-    ];
-    const movingForward = event.moved.newIndex > event.moved.oldIndex;
+
+    const itemBefore = distinctOrderedItems[event.moved.newIndex - 1] || orderedEntries[0][0];
+    const itemNext = distinctOrderedItems[event.moved.newIndex] || orderedEntries[orderedEntries.length - 1][0];
+    const indexOfItemBefore = orderedEntries.findIndex(it => it[0] === itemBefore);
+    const indexOfItemNext = orderedEntries.findIndex(it => it[0] === itemNext);
+    const newItemsBetween = orderedEntries.slice(indexOfItemBefore + 1, indexOfItemNext).map(i => i[0]);
+
+    // console.log(orderedEntries);
+    // console.log(itemBefore, indexOfItemBefore);
+    // console.log(itemNext, indexOfItemNext);
+    // console.log(itemReordered.value, itemsBetween.value);
+
+    if (newItemsBetween.length === 0) { 
+      if (event.moved.newIndex === 0) {
+        reorder(event.moved.element, itemBefore, false);
+      } else {
+        reorder(event.moved.element, itemNext, true);
+      }
+    } else {
+      itemReordered.value = event.moved.element;
+      itemsBetween.value = newItemsBetween;
+    }
+  }
+
+  function reorder(first: string, second: string, reverse = true) {
+    const orderedEntries = orderBy(Object.entries(shop.value), x => x[1]);
     const newOrderedEntries = orderedEntries
-      .filter(([key]) => key !== event.moved.element)
-      .flatMap(item =>
-        item[0] === distinctOrderedItems[event.moved.newIndex]
-          ? movingForward ? [item, newItemTuple] : [newItemTuple, item] 
-          : [item]
+      .map(tuple => tuple[0])
+      .filter(key => key !== first)
+      .flatMap(key => key === second
+        ? reverse ? [key, first] : [first, key]
+        : [key]
       )
-      .map(([key], index) => [key, index]);
+      .map((key, index) => [key, index]);
+    const newShopOrder = Object.fromEntries(newOrderedEntries);
+    db.collection("shops")
+      .doc(shopName)
+      .set(newShopOrder);
+    reset();
+  }
+
+  function removeFromList(item: string) {
     const newShopOrder = Object.fromEntries(
-      pushToLast ? [...newOrderedEntries, newItemTuple] : newOrderedEntries
+      Object.entries(shop.value).filter(([key]) => key !== item)
     );
+    itemsBetween.value = itemsBetween.value.filter(key => key !== item);
     db.collection("shops")
       .doc(shopName)
       .set(newShopOrder);
   }
 
   return {
-    onReorder
+    onReorder,
+    reorder,
+    removeFromList
   }
 }
